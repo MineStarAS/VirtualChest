d7ee180289a32357172b75db514fde81fb8b76a9
MineStarAS <band1019@naver.com>
2021-11-10 07:09:12 +0900

    # jitpack에서 maven으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,80 @@
 plugins {
     kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
 }
 
 group = "kr.kro.minestar"
 version = "1.0.0"
+
+tasks {
+    compileKotlin{
+        kotlinOptions.jvmTarget = "16"
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    create<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+        from("$buildDir/dokka/html")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("${rootProject.name}-api") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            repositories {
+                maven {
+                    name = "MavenCentral"
+                    val releasesRepoUrl = "https://repo.projecttl.net/repository/maven-releases/"
+                    val snapshotsRepoUrl = "https://repo.projecttl.net/repository/maven-snapshots/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+                    credentials.runCatching {
+                        username = project.properties["username"] as String?
+                        password = project.properties["password"] as String?
+                    }
+                }
+
+                pom {
+                    name.set(rootProject.name)
+                    description.set("This is minecraft default project")
+                    url.set("https://github.com/MineStarAS/MineCraftDefaultProject")
+                    licenses {
+                        license {
+                            name.set("GNU GENERAL PUBLIC LICENSE Version 3")
+                            url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("MineStarAS")
+                            name.set("MineStar")
+                            email.set("band1019@naver.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/MineStarAS/MineCraftDefaultProject.git")
+                        developerConnection.set("scm:git:https://github.com/MineStarAS/MineCraftDefaultProject.git")
+                        url.set("https://github.com/MineStarAS/MineCraftDefaultProject.git")
+                    }
+                }
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -13,7 +82,7 @@ repositories {
     maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
         name = "sonatype-oss-snapshots"
     }
-    maven(url = "https://jitpack.io/")
+    maven("https://repo.projecttl.net/repository/maven-public/")
 }
 
 dependencies {
@@ -21,45 +90,8 @@ dependencies {
     implementation("net.kyori:adventure-api:4.7.0")
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 
-    implementation("org.reflections:reflections:0.9.12")
-
     //project_TL
 
     //MineStar
 }
 
-tasks{
-    compileKotlin{
-        kotlinOptions.jvmTarget = "16"
-    }
-    processResources {
-        filesMatching("*.yml") {
-            expand(project.properties)
-        }
-    }
-    create<Jar>("sourcesJar") {
-        archiveClassifier.set("source")
-        from(sourceSets["main"].allSource)
-    }
-    shadowJar {
-        archiveBaseName.set(project.name)
-        archiveClassifier.set("")
-        archiveVersion.set(project.version.toString())
-        doLast {
-            // jar file copy
-            copy {
-                from(archiveFile)
-                val plugins = File("C:\\Users\\MineStar\\Desktop\\MC Server folder\\MCserver 1.17.1 - vanilla\\plugins")
-                into(if (File(plugins, archiveFileName.get()).exists()) plugins else plugins)
-            }
-        }
-    }
-}
-publishing {
-    publications {
-        create<MavenPublication>(project.name) {
-            artifact(tasks["sourcesJar"])
-            from(components["java"])
-        }
-    }
-}
